827114ef538157d53ad2651d7f154eaaf50cfd44
Prachi-kumari-1111 <prachikri8840@gmail.com>
2025-04-14 10:39:49 +0530

    e:\DSA starter\DSA\BasicPractiseQ\binarysearchtree.cpp

---
+++ BasicPractiseQ/binarysearchtree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// Definition of a BST node
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+// Insert a value into the BST
+TreeNode* insert(TreeNode* root, int val) {
+    if (!root) {
+        return new TreeNode(val);
+    }
+    if (val < root->val) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
+    }
+    return root;
+}
+
+// Inorder traversal (prints values in sorted order)
+void inorder(TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+
+int main() {
+    TreeNode* root = nullptr;
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    cout << "Inorder traversal of BST: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
