7e108e8a893072dab85f2f93452c1196dfee8ea9
Prachi-kumari-1111 <prachikri8840@gmail.com>
2025-09-08 21:48:27 +0530

    e:\DSA starter\DSA\LeetCodeEasyQ\sorted_arrayy.cpp

---
+++ LeetCodeEasyQ/sorted_arrayy.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Function for Binary Search
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == key) {
+            return mid; // element found
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1; // search in right half
+        }
+        else {
+            high = mid - 1; // search in left half
+        }
+    }
+    return -1; // element not found
+}
+
+int main() {
+    int n, key;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found!" << endl;
+
+    return 0;
+}
