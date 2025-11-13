91944246430a9a098d9446fc992d74a1231808a6
Prachi-kumari-1111 <prachikri8840@gmail.com>
2025-11-13 21:20:56 +0530

    e:\DSA starter\DSA\LeetCodeEasyQ\balance_parranthesis.cpp

---
+++ LeetCodeEasyQ/balance_parranthesis.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> s;
+    for (char ch : expr) {
+        if (ch == '(' || ch == '{' || ch == '[')
+            s.push(ch);
+        else {
+            if (s.empty()) return false;
+            char top = s.top();
+            s.pop();
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '['))
+                return false;
+        }
+    }
+    return s.empty();
+}
+
+int main() {
+    string expression;
+    cout << "Enter an expression: ";
+    cin >> expression;
+
+    if (isBalanced(expression))
+        cout << " Balanced Parentheses\n";
+    else
+        cout << "Not Balanced\n";
+
+    return 0;
+}
